7297c38c0b9aa5d529cb5ba071c03732c3010a82
falcoe <yangerben1996@live.com>
2018-09-03 21:35:12 +0800

    format code

---
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstring.java
@@ -0,0 +1,11 @@
+package LongestSubstringWithoutRepeatingCharacters;
+
+public class LongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
