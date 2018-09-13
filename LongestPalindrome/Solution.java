34e7ffa898cf5c5c7d48cf5af9207071ee297032
falcoe <yangerben1996@live.com>
2018-09-13 15:44:05 +0800

    409. Longest Palindrome

--- LongestPalindrome/Solution.java
+++ LongestPalindrome/Solution.java
@@ -7,6 +7,9 @@ package LongestPalindrome;
  * @create: 2018-09-13 13:56
  **/
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 /**
  * Given a string which consists of lowercase or uppercase letters, find the length of the longest
  * palindromes that can be built with those letters.
@@ -23,6 +26,48 @@ package LongestPalindrome;
  *
  * Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
  */
-public class Solution {
 
+class Solution {
+  public static int longestPalindrome(String s) {
+    char[] chars = s.toCharArray();
+    HashMap<Character, Integer> statistiques = new HashMap<>();
+    // Calculate the frequence of each character
+    for (int i = 0; i < chars.length; i++) {
+      Integer timer =  statistiques.get(chars[i]);
+      if (timer != null){
+        statistiques.put(chars[i], ++timer);
+      }else {
+        statistiques.put(chars[i], new Integer(1));
+      }
+    }
+    // add all even number， add one odd number
+    int length = 0;
+    Object key;
+    Integer value;
+    boolean singleFlag = false;
+    int oddMaxLength = 0;
+    Iterator iterator = statistiques.keySet().iterator();
+    while (iterator.hasNext()){
+      key = iterator.next();
+      value = statistiques.get(key);
+      if (value % 2 == 0){
+        length += value;
+      }else if(value == 1){
+        singleFlag = true;
+      }else if (value % 2 == 1 && value != 1){
+        length += value - 1;
+        singleFlag = true;
+      }
+    }
+    if (singleFlag){
+      length ++;
+    }
+    length += oddMaxLength;
+    return length;
+  }
+
+  public static void main(String[] args) {
+    String s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+    System.out.println(longestPalindrome(s));
+  }
 }
