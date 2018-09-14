6c23cd41bcfb618ca9bbe9f7362836399444e81b
falcoe <yangerben1996@live.com>
2018-09-14 16:50:17 +0800

    399. Evaluate Division

--- EvaluateDivision/Solution.java
+++ EvaluateDivision/Solution.java
@@ -8,18 +8,20 @@ package EvaluateDivision;
  **/
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
 /**
  * Equations are given in the format A / B = k, where A and B are variables represented as strings,
  * and k is a real number (floating point number). Given some queries, return the answers. If the
  * answer does not exist, return -1.0.
  *
- * Example:
- * Given a / b = 2.0, b / c = 3.0.
- * queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?,x / x = ?
- * return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+ * Example: Given a / b = 2.0, b / c = 3.0. queries are: a / c = ?, b / a = ?, a / e = ?, a / a =
+ * ?,x / x = ? return [6.0, 0.5, -1.0, 1.0, -1.0 ].
  *
  * The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string,
  * string>> queries , where equations.size() == values.size(), and the values are positive. This
@@ -27,79 +29,60 @@ import java.util.List;
  *
  * According to the example above:
  *
- * equations = [ ["a", "b"], ["b", "c"] ], values = [2.0, 3.0],
- * queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
- * The input is always valid. You may assume that evaluating the queries will result in no division
- * by zero and there is no contradiction.
+ * equations = [ ["a", "b"], ["b", "c"] ], values = [2.0, 3.0], queries = [ ["a", "c"], ["b", "a"],
+ * ["a", "e"], ["a", "a"], ["x", "x"] ]. The input is always valid. You may assume that evaluating
+ * the queries will result in no division by zero and there is no contradiction.
  */
 
+/**
+ * 参考： https://zxi.mytechroad.com/blog/graph/leetcode-399-evaluate-division/
+ */
 class Solution {
+
+  Map<String, HashMap<String, Double>> g = new HashMap<>();
+
   public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+    for (int i = 0; i < equations.length; ++i) {
+      String x = equations[i][0];
+      String y = equations[i][1];
+      double k = values[i];
+      g.computeIfAbsent(x, l -> new HashMap<String, Double>()).put(y, k);
+      g.computeIfAbsent(y, l -> new HashMap<String, Double>()).put(x, 1.0 / k);
+    }
 
-    // 准备数据
-    List<StructData> relations = new LinkedList<>();
-    HashMap<String ,Integer> existedCharacter = new HashMap<>();
-    if (equations.length > 0){
-      for (int i = 0; i < equations.length; i++) {
-        String[] tempEqualtions = equations[i];
-        StructData temp = new StructData();
-        temp.setValue(values[i]);
-        // 第一位是被除数
-        temp.setLeft(tempEqualtions[0]);
-        // 第二位是除数
-        temp.setRight(tempEqualtions[1]);
-        relations.add(temp);
-        // 统计所有出现的字符
-        existedCharacter.put(tempEqualtions[0],1);
-        existedCharacter.put(tempEqualtions[1],1);
+    double[] answer = new double[queries.length];
+
+    for (int i = 0; i < queries.length; ++i) {
+      String x = queries[i][0];
+      String y = queries[i][1];
+
+      if (!g.containsKey(x) || !g.containsKey(y)) {
+        answer[i] = -1.0;
+      } else {
+        answer[i] = divide(x, y, new HashSet<String>());
       }
     }
-    double[] result = new double[queries.length];
-
-    for (int i = 0; i < queries.length; i++) {
-      String[] queryParameter = queries[i];
-      String left = queryParameter[0];
-      String right = queryParameter[1];
-      // 查询的字符不存在
-      if (existedCharacter.get(left) == null || existedCharacter.get(right) == null){
-        result[i] = -1.0;
-      }
-
-      if (left.equals(right)){
-        result[i] = 1.0;
-      }
-      // 查询的字符需要运算
-      for (int j = 0; j < relations.size(); j++) {
-        StructData temp = relations.get(j);
-        if (temp.getLeft().equals(left) || temp.getRight().equals(left))
-      }
-
-    }
-    
-    return result;
-  }
-  class StructData{
-    Double value;
-    String left;
-    String right;
-    public void setValue(Double value){
-      this.value = value;
-    }
-    public void setLeft(String left){
-      this.left = left;
-    }
-    public void setRight(String right){
-      this.right = right;
-    }
-    public Double getValue(){
-      return this.value;
-    }
-    public String getLeft(){
-      return this.left;
-    }
-    public String getRight(){
-      return this.right;
-    }
+    return answer;
   }
 
+  private double divide(String x, String y, Set<String> visited) {
+    if (x.equals(y)) {
+      return 1.0;
+    }
+    visited.add(x);
+    if (!g.containsKey(x)) {
+      return -1.0;
+    }
+    for (String n : g.get(x).keySet()) {
+      if (visited.contains(n)) {
+        continue;
+      }
+      visited.add(n);
+      double d = divide(n, y, visited);
+      if (d > 0) {
+        return d * g.get(x).get(n);
+      }
+    }
+    return -1.0;
+  }
 }
